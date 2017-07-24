eba9fca28f0f042e3b5892682ca23b81d45c1063
amitoshseu <amitosh_cse_seu@yahoo.com>
2017-07-25 03:30:06 +0600

    Opencart Project first Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Store/admin/');
+define('HTTP_CATALOG', 'http://localhost/Store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Store/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Store/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/Store/admin/');
+define('DIR_SYSTEM', '/var/www/html/Store/system/');
+define('DIR_IMAGE', '/var/www/html/Store/image/');
+define('DIR_LANGUAGE', '/var/www/html/Store/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/Store/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/Store/system/config/');
+define('DIR_CACHE', '/var/www/html/Store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/Store/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/Store/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/Store/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/Store/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/Store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mrgain');
+define('DB_DATABASE', 'openCart_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Store/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/Store/catalog/');
+define('DIR_SYSTEM', '/var/www/html/Store/system/');
+define('DIR_IMAGE', '/var/www/html/Store/image/');
+define('DIR_LANGUAGE', '/var/www/html/Store/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/Store/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/Store/system/config/');
+define('DIR_CACHE', '/var/www/html/Store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/Store/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/Store/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/Store/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/Store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mrgain');
+define('DB_DATABASE', 'openCart_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
